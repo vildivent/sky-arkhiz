f23cf7a031478a797d4b975a5bcadb7c8a40018a
vildivent <vildivent@mail.ru>
2022-09-03 13:28:59 +0300

    Btn fix

--- components/CTACard.jsx
+++ components/CTACard.jsx
@@ -11,13 +11,13 @@ const CTACard = ({ heading, description, img, alt, link, opacity }) => {
         <div className={`flex flex-col justify-around font-h1 z-[1] mx-10`}>
           <h1 className={`text-[27px]`}>{heading}</h1>
           <span className={`text-[20px]`}>{description}</span>
-          <button
-            className={`hover:text-cyan-500 hover:top-[2px] hover:relative border border-cyan-500 rounded-full py-3`}
-          >
-            <Link href={link || "/"}>
+          <Link href={link || "/"}>
+            <button
+              className={`hover:text-cyan-500 hover:top-[2px] hover:relative border border-cyan-500 rounded-full py-3`}
+            >
               <a>Подробнее</a>
-            </Link>
-          </button>
+            </button>
+          </Link>
         </div>
 
         <div className={`opacity-${opacity} z-0`}>
