391d130c7c2e68e394c185c3c320e41fb015d837
vildivent <vildivent@mail.ru>
2022-10-07 11:28:15 +0300

    feat: implemented server props in news

--- pages/news/index.jsx
+++ pages/news/index.jsx
@@ -1,32 +1,29 @@
-import Image from "next/image";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 import PostItem from "../../components/PostItem";
 import { MainLayout } from "../../layouts/MainLayout";
-import { loadingGif } from "../../public/assets";
-import { getAllPosts } from "../../redux/features/post/postSlice";
 
-export default function News() {
-  const dispatch = useDispatch();
-  const { posts, loading } = useSelector((state) => state.post);
+const path = process.env.NEXT_PUBLIC_API_URL;
 
-  useEffect(() => {
-    dispatch(getAllPosts());
-  }, [dispatch]);
+export async function getServerSideProps() {
+  try {
+    const { data } = await axios.get(`${path}api/posts/getAll`);
+    return {
+      props: { posts: data.posts },
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
 
+export default function News({ posts }) {
   return (
     <MainLayout title={"Новости"} mainProps={"px-2"}>
       <div className={`flex flex-col flex-wrap items-center gap-3 mt-3`}>
-        {!posts.length && (
-          <>
-            {loading ? (
-              <Image src={loadingGif} alt="loading" width={40} height={40} />
-            ) : (
-              <span>Новостей нет</span>
-            )}
-          </>
+        {posts ? (
+          posts.map((post) => <PostItem key={post._id} post={post} />)
+        ) : (
+          <span>Новостей нет</span>
         )}
-        {posts && posts.map((post) => <PostItem key={post._id} post={post} />)}
       </div>
     </MainLayout>
   );
