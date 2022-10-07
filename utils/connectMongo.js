be73f7a410f359e21ea4778d0e4a01db96211526
vildivent <vildivent@mail.ru>
2022-10-07 12:24:37 +0300

    fix

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
 const connectMongo = async () =>
-  mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI);
+  mongoose.connect(
+    "mongodb+srv://admin:admin135@cluster0.lurv3cr.mongodb.net/sky-arkhyz?retryWrites=true&w=majority"
+  );
 export default connectMongo;
