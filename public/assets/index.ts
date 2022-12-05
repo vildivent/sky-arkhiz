a02517ec82fc72cd162c38c591f98e9c62bb8951
vildivent <vildivent@mail.ru>
2022-12-06 02:25:34 +0300

    remove old imports

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,54 +1,42 @@
 import mainBg from "./bg.jpg";
-import btaBg1 from "./btaBg1.jpg";
-import bgStars4k from "./bg-stars-4k.png";
-import closeWhiteIcon from "./close-white.svg";
-import closeCyanIcon from "./close-cyan500.svg";
-import menuWhiteIcon from "./menu-white.svg";
-import menuCyanIcon from "./menu-cyan500.svg";
+import mobileBg from "./mobile-bg.jpg";
+import bta360Mobile from "./BTA_8mm_15jul2021_360min_ed3flat_1531x3400.jpg";
+import btaSquare from "./BTA_8mm_15jul2021_360min_square.jpg";
+
+import celestron from "./Celestron.png";
+import cameraPreview from "./camera-preview.png";
+import roadToStarsGalaxy from "./road-to-stars-galaxy.png";
+import roadToStarsTelescope from "./road-to-stars-telescope.png";
+import bta1 from "./bta1.jpg";
+import bta2 from "./bta2.jpg";
+import ratan600 from "./ratan600.jpg";
+import planetarium from "./planetarium.jpg";
+import planetariumSmall from "./planetarium-small.png";
+import btaSmall from "./bta-small.png";
+
+import loadingGif from "./loading.svg";
+import transparentPlaceholder from "./transparent-placeholder.png";
+
 import telescopeLogo from "./telescope-logo1.svg";
 import telescopeLogoCyan from "./telescope-logo1-cyan.svg";
-import roadToStarsGalaxy from "./road-to-stars-galaxy.png";
-import roadToStarsGalaxyCut from "./road-to-stars-galaxy-cut.png";
-import roadToStarsTelescope from "./road-to-stars-telescope.png";
-import loadingGif from "./loading.svg";
-import mobileBg from "./mobile-bg.jpg";
 import backArrowCyan500 from "./back-arrow-cyan500.svg";
 import backArrowWhite from "./back-arrow-white.svg";
+
 import iconTelegram from "./icon-telegram-app.svg";
 import iconWhatsapp from "./icon-whatsapp.svg";
 import iconVk from "./icon-vk-circled.svg";
 import iconInstagram from "./icon-instagram.svg";
 import iconNowapp from "./icon-nowapp.png";
 import iconPhone from "./icon-phone.svg";
-import bta1 from "./bta1.jpg";
-import bta2 from "./bta2.jpg";
-import ratan600 from "./ratan600.jpg";
-import planetarium from "./planetarium.jpg";
-import btaMobile from "./bta-mobile.webp";
-import celestron from "./Celestron.png";
+
 import cardImage1 from "./cardImage1.png";
-import cameraPreview from "./camera-preview.png";
-import planetariumSmall from "./planetarium-small.png";
-import btaSmall from "./bta-small.png";
-import transparentPlaceholder from "./transparent-placeholder.png";
-import bta360Mobile from "./BTA_8mm_15jul2021_360min_ed3flat_1531x3400.jpg";
-import bta360 from "./BTA_8mm_15jul2021_360min_ed3flat_2695x1516.jpg";
-import btaOriginal from "./BTA_8mm_15jul2021_360min_ed3flat.jpg";
-import btaSquare from "./BTA_8mm_15jul2021_360min_square.jpg";
 
 export {
   mainBg,
-  btaBg1,
-  bgStars4k,
-  closeWhiteIcon,
-  closeCyanIcon,
-  menuWhiteIcon,
-  menuCyanIcon,
   telescopeLogo,
   telescopeLogoCyan,
   roadToStarsGalaxy,
   roadToStarsTelescope,
-  roadToStarsGalaxyCut,
   loadingGif,
   mobileBg,
   backArrowCyan500,
@@ -63,7 +51,6 @@ export {
   bta2,
   ratan600,
   planetarium,
-  btaMobile,
   celestron,
   cardImage1,
   cameraPreview,
@@ -71,7 +58,5 @@ export {
   btaSmall,
   transparentPlaceholder,
   bta360Mobile,
-  bta360,
-  btaOriginal,
   btaSquare,
 };
